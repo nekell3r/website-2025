36edef0fcc0e3dad5c634e79589be54ed2e650ca
nekell3r <pazhabskiy032007@gmail.com>
2025-03-24 22:42:23 +0300

    подключил бд, начал работу с тестовыми таблицами через alembic и sqlalchemy

---
+++ src/models/reviews.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+
+from src.database import Base
+
+
+class ReviewsORM(Base):
+    __tablename__ = "reviews"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(25))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    exam: Mapped[str] = mapped_column(String(10))
+    result: Mapped[int] = mapped_column()
+    review: Mapped[str] = mapped_column(String(500))
---
+++ src/models/users.py
@@ -0,0 +1,16 @@
+from pydantic import EmailStr
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+from src.database import Base
+
+
+class UsersORM(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(25))
+    email: Mapped[EmailStr | None] = mapped_column()
+    telephone: Mapped[str] = mapped_column(String(15))
+    password: Mapped[str] = mapped_column(String(50))
+    exam: Mapped[str] = mapped_column(String(10))
