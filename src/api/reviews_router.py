8ee3ba514cdef5456ed1fe996f87f16ba6f32c0e
nekell3r <pazhabskiy032007@gmail.com>
2025-03-25 13:17:25 +0300

    добавил возможность получать данные из бд

--- src/api/reviews_router.py
+++ src/api/reviews_router.py
@@ -2,7 +2,7 @@ import sys
 
 from fastapi import APIRouter, Body
 from pathlib import Path
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 
 from src.database import async_session_maker
 from src.schemas.schemas import ReviewSchema
@@ -13,8 +13,42 @@ sys.path.append(str(Path(__file__).parent.parent))
 router = APIRouter(prefix="/reviews", tags=["reviews"])
 
 
+@router.get("/")
+async def get_reviews():
+    async with async_session_maker() as session:
+        query = select(ReviewsORM)
+        result = await session.execute(query)
+        reviews = result.scalars().all()
+        return reviews
+
+
 @router.post("/")
-async def create_review(schema: ReviewSchema = Body()):
+async def create_review(
+    schema: ReviewSchema = Body(
+        openapi_examples={
+            "1": {
+                "summary": "Отзыв Павла",
+                "value": {
+                    "user_id": 3,
+                    "name": "Павел",
+                    "exam": "ЕГЭ",
+                    "result": 100,
+                    "review": "Замечательный учитель",
+                },
+            },
+            "2": {
+                "summary": "Отзыв Антона",
+                "value": {
+                    "user_id": 4,
+                    "name": "Павел",
+                    "exam": "ЕГЭ",
+                    "result": 80,
+                    "review": "кайфово было вообще блин",
+                },
+            },
+        }
+    )
+):
     async with async_session_maker() as session:
         statemant_add_review = insert(ReviewsORM).values(**schema.model_dump())
         await session.execute(statemant_add_review)
